570768d339f4fa8650fc548b78c7584d8d342c4d
arunprazad001 <arunprazad001@gmail.com>
2022-11-09 23:25:36 +0530

    updated day2

--- Online_Plant_Nursery_App/src/main/java/com/plantapp/exception/GlobalExceptionHandler.java
+++ Online_Plant_Nursery_App/src/main/java/com/plantapp/exception/GlobalExceptionHandler.java
@@ -16,30 +16,9 @@ import org.springframework.web.context.request.WebRequest;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-	@ExceptionHandler(SeedException.class)
-	public ResponseEntity<MyErrorDetail> studentExceptionHandler(SeedException se, WebRequest req) {
-		
-		MyErrorDetail err= new MyErrorDetail();
-		err.setTimestamp(LocalDateTime.now());
-		err.setMessage(se.getMessage());
-		err.setDescription(req.getDescription(false));
-		
-		return new ResponseEntity<MyErrorDetail>(err,HttpStatus.BAD_REQUEST);
-		
-	}
 	
 	
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<MyErrorDetail> ExceptionHandler(Exception se, WebRequest req) {
-		
-		MyErrorDetail err= new MyErrorDetail();
-		err.setTimestamp(LocalDateTime.now());
-		err.setMessage(se.getMessage());
-		err.setDescription(req.getDescription(false));
-		
-		return new ResponseEntity<MyErrorDetail>(err,HttpStatus.BAD_REQUEST);
-		
-	}
+	
 	
 	
 	
