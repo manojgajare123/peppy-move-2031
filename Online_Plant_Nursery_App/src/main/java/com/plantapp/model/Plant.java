298023e20374061715d77254d65e283dc1cb4200
manoj gajare <manojgajare5@gmail.com>
2022-11-11 15:36:30 +0530

    Day4 Task

--- Online_Plant_Nursery_App/src/main/java/com/plantapp/model/Plant.java
+++ Online_Plant_Nursery_App/src/main/java/com/plantapp/model/Plant.java
@@ -1,11 +1,22 @@
 package com.plantapp.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Plant {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -28,126 +39,13 @@ public class Plant {
 	private String plantDescription;
 	private Integer plantStock;
 	private Double plantCost;
-
-	public Plant() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public Plant(Integer plantId, Integer plantHeight, String plantSpread, String commonName, String bloomTime,
-			String medicinalOrCulinaryUse, String temparature, String typeOfPlant, String plantDescription,
-			Integer plantStock, Double plantCost) {
-		super();
-		this.plantId = plantId;
-		this.plantHeight = plantHeight;
-		this.plantSpread = plantSpread;
-		this.commonName = commonName;
-		this.bloomTime = bloomTime;
-		this.medicinalOrCulinaryUse = medicinalOrCulinaryUse;
-		this.temparature = temparature;
-		this.typeOfPlant = typeOfPlant;
-		this.plantDescription = plantDescription;
-		this.plantStock = plantStock;
-		this.plantCost = plantCost;
-	}
-
-
-	public Integer getPlantId() {
-		return plantId;
-	}
-
-	public void setPlantId(Integer plantId) {
-		this.plantId = plantId;
-	}
-
-	public Integer getPlantHeight() {
-		return plantHeight;
-	}
-
-	public void setPlantHeight(Integer plantHeight) {
-		this.plantHeight = plantHeight;
-	}
-
-	public String getPlantSpread() {
-		return plantSpread;
-	}
-
-	public void setPlantSpread(String plantSpread) {
-		this.plantSpread = plantSpread;
-	}
-
-	public String getCommonName() {
-		return commonName;
-	}
-
-	public void setCommonName(String commonName) {
-		this.commonName = commonName;
-	}
-
-	public String getBloomTime() {
-		return bloomTime;
-	}
-
-	public void setBloomTime(String bloomTime) {
-		this.bloomTime = bloomTime;
-	}
-
-	public String getMedicinalOrCulinaryUse() {
-		return medicinalOrCulinaryUse;
-	}
-
-	public void setMedicinalOrCulinaryUse(String medicinalOrCulinaryUse) {
-		this.medicinalOrCulinaryUse = medicinalOrCulinaryUse;
-	}
-
-	public String getTemparature() {
-		return temparature;
-	}
-
-	public void setTemparature(String temparature) {
-		this.temparature = temparature;
-	}
-
-	public String getTypeOfPlant() {
-		return typeOfPlant;
-	}
-
-	public void setTypeOfPlant(String typeOfPlant) {
-		this.typeOfPlant = typeOfPlant;
-	}
-
-	public String getPlantDescription() {
-		return plantDescription;
-	}
-
-	public void setPlantDescription(String plantDescription) {
-		this.plantDescription = plantDescription;
-	}
-
-	public Integer getPlantStock() {
-		return plantStock;
-	}
-
-	public void setPlantStock(Integer plantStock) {
-		this.plantStock = plantStock;
-	}
-
-	public Double getPlantCost() {
-		return plantCost;
-	}
-
-	public void setPlantCost(Double plantCost) {
-		this.plantCost = plantCost;
-	}
-
-	@Override
-	public String toString() {
-		return "Plant [plantId=" + plantId + ", plantHeight=" + plantHeight + ", plantSpread=" + plantSpread
-				+ ", commonName=" + commonName + ", bloomTime=" + bloomTime + ", medicinalOrCulinaryUse="
-				+ medicinalOrCulinaryUse + ", temparature=" + temparature + ", typeOfPlant=" + typeOfPlant
-				+ ", plantDescription=" + plantDescription + ", plantStock=" + plantStock + ", plantCost=" + plantCost
-				+ "]";
-	}
-
+	
+	@JsonIgnore
+	@ManyToOne (cascade = CascadeType.ALL)
+	private Planter planter;
+	
 }
+	
+	
 
+	
--- Online_Plant_Nursery_App/src/main/java/com/plantapp/model/Planter.java
+++ Online_Plant_Nursery_App/src/main/java/com/plantapp/model/Planter.java
@@ -1,12 +1,16 @@
 package com.plantapp.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -49,5 +53,11 @@ public class Planter {
 	@Min(value = 0,message ="Cost should be greater than 0" )
 	private Integer planterCost;
 	
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "plantId")
+	private List<Plant> plants= new ArrayList<>();
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "seedId")
+	private List<Seed> seeds= new ArrayList<>();
 	
 }
