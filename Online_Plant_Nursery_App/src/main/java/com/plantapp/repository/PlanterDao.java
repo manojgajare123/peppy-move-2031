8d693050a40ec2ff718b84b85d85b7c20b34b9fa
sumitbokshi <bokshisumit@gmail.com>
2022-11-11 23:49:43 +0530

    day-4

--- Online_Plant_Nursery_App/src/main/java/com/plantapp/repository/PlanterDao.java
+++ Online_Plant_Nursery_App/src/main/java/com/plantapp/repository/PlanterDao.java
@@ -3,21 +3,17 @@ package com.plantapp.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.plantapp.model.Plant;
 import com.plantapp.model.Planter;
-import com.plantapp.model.Seed;
 
 @Repository
 public interface PlanterDao extends JpaRepository<Planter, Integer> {
 
-	List<Planter> findByPlanterShape(String planterShape);
 	
+		
 	
-
-		
-		
 		
 }
 
--- Online_Plant_Nursery_App/src/main/java/com/plantapp/service/PlanterServiceImpl.java
+++ Online_Plant_Nursery_App/src/main/java/com/plantapp/service/PlanterServiceImpl.java
@@ -1,85 +1,67 @@
 package com.plantapp.service;
 
+
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.plantapp.exception.PlanterException;
-import com.plantapp.model.Plant;
 import com.plantapp.model.Planter;
-import com.plantapp.model.Seed;
-import com.plantapp.repository.PlantDao;
 import com.plantapp.repository.PlanterDao;
 
+
 @Service
-public class PlanterServiceImpl implements PlanterService {
+public class PlanterServiceImpl implements PlanterService{
 
 	@Autowired
 	private PlanterDao planterDao;
-
-	@Autowired
-	private PlantDao plantDao;
-
+	
+	
 	@Override
 	public Planter addPlanter(Planter planter) throws PlanterException {
+		Planter addPlanter=planterDao.save(planter);
 
-		List<Plant> plant = planter.getPlants();
-
-		List<Seed> seeds = planter.getSeeds();
-
-		if (!seeds.isEmpty()) {
-
-			for (Seed s : seeds) {
-
-				s.setPlanter(planter);
-			}
-
-		}
-
-		if (!plant.isEmpty()) {
-
-			for (Plant p : plant) {
-
-				p.setPlanter(planter);
-			}
-
-		}
-
-		Planter addPlanter = planterDao.save(planter);
-
-		if (addPlanter != null)
-
-		{
-
-//        	System.out.println(addPlanter.toString());
-			return addPlanter;
-		}
-
-		else {
-			throw new PlanterException("Plant details are empty........");
+        if(addPlanter!=null)
+        {
+        	return addPlanter;
+        }
+         
+        
+        else {
+        throw new PlanterException("Plant details are empty........");
 		}
 
 	}
 
 	@Override
 	public Planter updatePlanter(Planter planter) throws PlanterException {
-
+		
 		planterDao.findById(planter.getPlanterId()).orElseThrow(() -> new PlanterException("Planter  not found"));
-
+        
 		return planterDao.save(planter);
-
+	        
 	}
 
 	@Override
 	public Planter deletePlanter(Planter planter) throws PlanterException {
-		// TODO Auto-generated method stub
-		return null;
+
+		planterDao.findById(planter.getPlanterId()).orElseThrow(() 
+				    -> new PlanterException("Planter does not exist with Plant Id "+planter.getPlanterId()));;
+		
+		planterDao.delete(planter);
+				
+		return planter;
 	}
 
 	@Override
 	public Planter viewPlanterById(Integer planterId) throws PlanterException {
-		// TODO Auto-generated method stub
-		return null;
+
+		Optional<Planter> opt= planterDao.findById(planterId);
+		
+		return opt.orElseThrow(() 
+				    -> new PlanterException("Planter not exist with Id :"+ planterId) );
 	}
 
 	@Override
@@ -90,8 +72,15 @@ public class PlanterServiceImpl implements PlanterService {
 
 	@Override
 	public List<Planter> viewAllPlanters() throws PlanterException {
-		// TODO Auto-generated method stub
-		return null;
+
+		List<Planter> planters = planterDao.findAll();
+		
+		if(planters.size() > 0) {
+			return planters;
+		}
+		else {
+			throw new PlanterException("No Planters Found");
+		}
 	}
 
 	@Override
@@ -99,5 +88,8 @@ public class PlanterServiceImpl implements PlanterService {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	
+	
 
 }
