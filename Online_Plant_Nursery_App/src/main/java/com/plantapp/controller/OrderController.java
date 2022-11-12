3ce02f35747018acf6435708e40ddf84ffefc1ea
Rohit9252 <rohitbatra0165@gmail.com>
2022-11-12 10:44:57 +0530

    Order

--- Online_Plant_Nursery_App/src/main/java/com/plantapp/controller/OrderController.java
+++ Online_Plant_Nursery_App/src/main/java/com/plantapp/controller/OrderController.java
@@ -2,11 +2,17 @@ package com.plantapp.controller;
 
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.plantapp.exception.OrderException;
 import com.plantapp.model.Customer;
 import com.plantapp.model.CustomerDTO;
 import com.plantapp.model.PlantOrder;
@@ -24,11 +30,25 @@ public class OrderController {
 	@Autowired
 	private PlanterService pservice;
 
-	@PostMapping("/addorder")
-	public List<Planter> saveOrder(@RequestBody CustomerDTO cdto) {
+	@PostMapping("/addorder/{uuid}")
+	public PlantOrder saveOrder(@PathVariable("uuid") String uid, @RequestBody CustomerDTO cdto) throws OrderException {
 
-		return oService.placeOrder(cdto);
-		
+		PlantOrder order = oService.placeOrder(cdto, uid);
+
+		return order;
+
+	}
+
+	@GetMapping("/viewall")
+	public List<PlantOrder> viewAllorder() throws OrderException {
+
+		return oService.viewAllOrder();
+	}
+
+	@GetMapping("/order/{id}")
+	public PlantOrder getOrderbyId(@PathVariable("id") Integer id) throws OrderException {
+
+		return oService.deleteOrder(id);
 	}
 
 }
