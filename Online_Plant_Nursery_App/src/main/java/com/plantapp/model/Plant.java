274907a90c4bb8b5936927f7587452959f6b4095
manoj gajare <manojgajare5@gmail.com>
2022-11-12 17:12:49 +0530

    Day5 Task

--- Online_Plant_Nursery_App/src/main/java/com/plantapp/model/Plant.java
+++ Online_Plant_Nursery_App/src/main/java/com/plantapp/model/Plant.java
@@ -7,6 +7,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -25,25 +29,43 @@ public class Plant {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer plantId;
-
-	private Integer plantHeight;
-
-	private String plantSpread;
-
-	private String commonName;
-
-	private String bloomTime;
-
-	private String medicinalOrCulinaryUse;
-
-	private String temparature;
-
-	private String typeOfPlant;
-
-	private String plantDescription;
-	private Integer plantStock;
-	private Double plantCost;
 	
+	@NotNull(message = "Height of the plant must be there")
+	@Min(value = 1, message = "Plant height should be greater than zero")
+	private Integer plantHeight;
+	
+	@NotEmpty(message = "Plant Spread must be there")
+	private String plantSpread;
+	
+	@NotEmpty(message = "Common name of plant is missing")
+	private String commonName;
+	
+	@NotEmpty(message = "Bloom time for plant need to mention")
+	private String bloomTime;
+	
+	@NotEmpty(message = "Use of plant must be there")
+	private String medicinalOrCulinaryUse;
+	
+	@NotEmpty(message = "Difficulty level for plant should be there")
+	private String difficultyLevel;
+	
+	@NotEmpty(message = "Temparature need to mention")
+	private String temparature;
+	
+	@NotEmpty(message = "Plant type is missing")
+	private String typeOfPlant;
+	
+	@NotEmpty(message = "Plant description should not be empty")
+	@Size(min = 20, max = 500, message = "Minimun plant description size must be greater than 20 and less than 500")
+	private String plantDescription;
+	
+	@NotNull(message = "Stock size must be there")
+	@Min(value = 1, message = "Plant stock must be greate than 0")
+	private Integer plantStock;
+	
+	@NotNull(message = "Plant cost should be there")
+	@Min(value = 1, message = "Plant cost must be greate than 0")
+	private Double plantCost;
 	
 	@ManyToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name ="planterId")
